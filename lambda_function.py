54d873ea0394e66403923582b228a60c1f7eda1c
neryaelul <neryaelul@gmail.com>
2022-09-19 01:45:55 +0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -23,57 +23,69 @@ class customersDB:
                 }
             )
         return True
-
 def lambda_handler(event, context):
-    customers = customersDB('customers_ids')
-
-    if event and event['httpMethod']:
-        if event['httpMethod'] == "PUT":
-            bodyJson2Arr = json.loads(event['body'])
-            if bodyJson2Arr['tasktype'] == "create":
-                if bodyJson2Arr['data']['id']:
-                    exist = customers.get(bodyJson2Arr['data']['id'])
-                    message = exist
-                    if not exist:
-                        customers.create(bodyJson2Arr['data']['id'])
-                        statusCode = 200
-                        code = 200
-                        resTypeStatus = 'OK'
-                        message = 'New User Id adding successfully'
-                    else:
-                        statusCode = 302
-                        resTypeStatus = 'OK'
-                        code = 302
-                        message = 'Sorry but this User alredy exist'
-                else:
-                    statusCode = 419
-                    code = 419
-                    resTypeStatus = 'MISSING ARGUMENTS'
-                    message = 'Sorry but not found any Id in your request'
-            else:
-                statusCode = 419
-                code = 419
-                resTypeStatus = 'MISSING ARGUMENTS'
-                message = 'Sorry but not found any Create in your request'
+    
+        customers = customersDB('customers_ids')
+        
+        if event and event['httpMethod']:
+            Authorization = event['headers'].get('Authorization', None)
+            if Authorization != None and event['headers']['Authorization'] == 'neryanerya':
                 
-        if event['httpMethod'] == 'GET':
-            if customers.get(event['multiValueQueryStringParameters']['id'][0]):
-                statusCode = 302
-                code = 302
-                resTypeStatus = 'OK'
-                message = 'This User exist'
+                if event['httpMethod'] == "PUT":
+                    bodyJson2Arr = json.loads(event['body'])
+                    if bodyJson2Arr['tasktype'] == "create":
+                        if bodyJson2Arr['data']['id']:
+                            exist = customers.get(bodyJson2Arr['data']['id'])
+                            message = exist
+                            if not exist:
+                                customers.create(bodyJson2Arr['data']['id'])
+                                statusCode = 200
+                                resTypeStatus = 'OK'
+                                message = 'New User Id adding successfully'
+                            else:
+                                statusCode = 302
+                                resTypeStatus = 'OK'
+                                message = 'Sorry but this User alredy exist'
+                        else:
+                            statusCode = 419
+                            resTypeStatus = 'MISSING ARGUMENTS'
+                            message = 'Sorry but not found any Id in your request'
+                    else:
+                        statusCode = 419
+                        resTypeStatus = 'MISSING ARGUMENTS'
+                        message = 'Sorry but not found any Create in your request'
+                        
+                elif event['httpMethod'] == 'GET':
+                    id = event['multiValueQueryStringParameters'].get('id', None)
+                    if id == None:
+                        statusCode = 419
+                        resTypeStatus = 'MISSING ARGUMENTS'
+                        message = 'Sorry but not found any Id in your request'
+                    else:    
+                        if customers.get(event['multiValueQueryStringParameters']['id'][0]):
+                            statusCode = 302
+                            resTypeStatus = 'OK'
+                            message = 'This User exist'
+                        else:
+                            statusCode = 404
+                            resTypeStatus = 'OK'
+                            message = 'This User not found ):'
+                else:
+                    statusCode = 405
+                    message= 'This method type is not currently supported.'
+                    resTypeStatus = 'METHOD NOT ALLOWED'
             else:
-                statusCode = 404
-                code = 404
-                resTypeStatus = 'OK'
-                message = 'This User not found ):'
-            
-        jsonDataBody = {'code': code, 'status': resTypeStatus,'message': message}
-                  
+                statusCode = 403
+                message= 'Failed to pass authorization.'
+                resTypeStatus = 'Forbidden'
+    
+        jsonDataBody = {'code': statusCode, 'status': resTypeStatus,'message': message}
+    
         return {
             'headers': {
                 "content-type":"application/json"
             },
-            'statusCode': 200,
+            'statusCode': statusCode,
             'body': json.dumps(jsonDataBody)
         }
+        
